0674b890c45595116fb451d75c1c6f497ae6ee71
Dmytro <dmutro15um10@gmail.com>
2025-07-25 19:39:24 +0200

    3-d homework

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,16 @@
-function getElementWidth(content, padding, border) {
-  let numContent = parseFloat(content);
-  let numPadding = parseFloat(padding);
-  let numBorder = parseFloat(border);
-  let fullWidth = numContent + (numPadding + numBorder) * 2;
-  return fullWidth;
+function filterArray(numbers, value) {
+  let numArray = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      numArray.push(numbers[i]);
+    }
+  }
+  return numArray;
 }
-console.log(getElementWidth('50px', '8px', '4px'));
-console.log(getElementWidth('60px', '12px', '8.5px'));
-console.log(getElementWidth('200px', '0px', '0px'));
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
